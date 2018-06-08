dec5f84cfa7da403dac3ab4b651fe3d50881d731
Dave Michelson <daveism@gmail.com>
2018-06-08 10:02:43 -0400

    fix for layer list poistion

--- src/scripts/maplayers_list.js
+++ src/scripts/maplayers_list.js
@@ -21,6 +21,10 @@ export class MapLayersList extends Component {
 
     // Add a toggle button for each layer
     WMSLayers.forEach((layerProps) => this.addMapLayer(layerProps))
+
+    var elem = document.getElementById(placeholderId);
+    elem.outerHTML = elem.innerHTML;
+
   }
 
 
