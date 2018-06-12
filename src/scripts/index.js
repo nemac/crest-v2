df364cf1819adc750b64d7b8143637cb1b8cf6e1
Dave Michelson <daveism@gmail.com>
2018-06-12 08:41:27 -0400

    need routing to handle state.
    tried to do seperate pages but it made the handling of state too complex.  created a router to be able to store state across page changes.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,72 +1,89 @@
-// import MapController from './mapController';
+//import dependencies
+import Navigo from 'navigo';
 
-import { Map } from './map';
-import { MapLayersList } from './maplayers_list';
-
-import { nav_bar } from './nav_bar';
+//import custom classess
 import { Store } from './store';
 
-import Navigo from 'navigo';
-//
-import AboutPage from '../templates/about.html'
-import DownloadDataPage from '../templates/downloaddata.html'
-// import AboutPage from '../templates/first.html'
-// import Navigo from 'navigo';
-// // const router = new Navigo(location.protocol + "//" + location.host,false,'#!')
-const router = new Navigo('http://localhost:8080', true, "#")
-//
+//import extended components
+import { Map } from './map';
+import { MapLayersList } from './maplayers_list';
+import { nav_bar } from './nav_bar';
 
+//import html templates
+import AboutPage from '../templates/about.html';
+import DownloadDataPage from '../templates/downloaddata.html';
+import NotFoundPage from '../templates/notfound.html';
 
-let state = new Store({test:"test"})
+//initialize classes
+const router = new Navigo('http://localhost:8080', true, "!#");
+let state = new Store({test:"test"});
+
 
 class ViewController {
 
     constructor () {
+
+      //setup navigo router.
       router.on({
-        '/map': ()=>{
-            console.log('map')
-            console.log('state',state)
-             this.initializeNavbar('index');
-             this.initializeMap();
+        //default route also map
+        '/': (params, query)=>{
 
-             },  // start with slash
-         '/about': ()=>{
-              // router.navigate('/admin');
-              console.log('about');
-              this.initializeNavbar('about');
+          //initialize navbar
+          this.initializeNavbar('index');
 
-              this.componentElem = document.getElementById("app")
-              this.componentElem.innerHTML = AboutPage
+          //initialize map
+          this.initializeMap();
 
-          },  // start with slash
-          '/downloaddata': ()=>{
-              console.log('downloaddata');
-              this.initializeNavbar('download');
+        },
+        //go to map page
+        '/map': (params, query)=>{
 
-              this.componentElem = document.getElementById("app")
-              this.componentElem.innerHTML = DownloadDataPage
+          //initialize navbar
+          this.initializeNavbar('index');
 
-                   },  // start with slash
-          '/test': ()=>{
-               // router.navigate('/admin');
-               console.log('test')
+          //initialize map
+          this.initializeMap();
 
-               },  // start with slash
-           '/': ()=>{
+        },
+        //go to about page
+        '/about': (params, query)=>{
 
-                console.log(state,'map')
+          //initialize navbar
+          this.initializeNavbar('about');
 
-                this.initializeNavbar('index');
-                this.initializeMap();
+          //initialize about
+          this.componentElem = document.getElementById("app");
+          this.componentElem.innerHTML = AboutPage;
 
-                },  // start with slash
+        },
+        //go to download data page
+        '/downloaddata': (params, query)=>{
 
-      }).resolve();
+          //initialize navbar
+          this.initializeNavbar('download');
+
+          //initialize download
+          this.componentElem = document.getElementById("app");
+          this.componentElem.innerHTML = DownloadDataPage;
+
+        },
+
+      })
+
+
+      router.notFound(function (query) {
+        //initialize 404
+        this.componentElem = document.getElementById("app");
+        this.componentElem.innerHTML = NotFoundPage;
+      });
+
+      //resolve
+      router.resolve();
+
     }
 
     initializeNavbar (page){
       this.navComponent = new nav_bar('nav-holder',{activeNav: page});
-      console.log(this.navComponent.activeNav)
     }
 
     initializeMap(){
@@ -85,162 +102,4 @@ class ViewController {
     }
 }
 
-window.ctrl = new ViewController()
-
-
-
-    // router.on({
-    //   '/map': ()=>{
-    //        // router.navigate('/admin');
-    //        console.log('map')
-    //
-    //        // window.ctrl = new mapController()
-    //        // console.log(mapComponent)
-    //
-    //        },  // start with slash
-    //    '/about': ()=>{
-    //         // router.navigate('/admin');
-    //         console.log('about');
-    //
-    //
-    //         // class ViewController {
-    //         //
-    //         //   /** Initialize Application */
-    //         //   constructor () {
-    //         //     this.initializeComponents()
-    //         //   }
-    //         //
-    //         //   initializeComponents () {
-    //         //     // Initialize Nav Var
-    //         //     this.navComponent = new nav_bar('nav-holder',{activeNav: 'about'});
-    //         //     console.log(this.navComponent.activeNav)
-    //         //
-    //         //     this.componentElem = document.getElementById("app")
-    //         //     this.componentElem.innerHTML = AboutPage
-    //         //   }
-    //         // }
-    //
-    //
-    //         window.ctrl = new ViewController()
-    //
-    //         },  // start with slash
-    //         '/downloaddata': ()=>{
-    //              // router.navigate('/admin');
-    //              console.log('about');
-    //
-    //
-    //              // class ViewController {
-    //              //
-    //              //   /** Initialize Application */
-    //              //   constructor () {
-    //              //     this.initializeComponents()
-    //              //   }
-    //              //
-    //              //   initializeComponents () {
-    //              //     // Initialize Nav Var
-    //              //     this.navComponent = new nav_bar('nav-holder',{activeNav: 'download'});
-    //              //     console.log(this.navComponent.activeNav)
-    //              //
-    //              //     this.componentElem = document.getElementById("app")
-    //              //     this.componentElem.innerHTML = DownloadDataPage
-    //              //   }
-    //              // }
-    //              //
-    //              //
-    //              // window.ctrl = new ViewController()
-    //
-    //              },  // start with slash
-    //     '/test': ()=>{
-    //          // router.navigate('/admin');
-    //          console.log('test')
-    //
-    //          },  // start with slash
-    //      '/': ()=>{
-    //           // router.navigate('/admin');
-    //           console.log('map')
-    //
-    //           // mapController
-    //
-    //           },  // start with slash
-    //
-    // }).resolve();
-
-// }
-// function mapDefault() {
-//
-// }
-// router
-//   .notFound( (q)=>{console.log(q,'not found')})
-//   .on(
-//     '/', (q)=>{console.log(q,'home')},
-//     '/about', (q)=>{console.log(q,'about')},
-//   )
-//   .resolve();
-
-// router.resolve();
-//
-// console.log()
-// router
-//     .on('/', HomePage)
-//     .on('/about', AboutPage)
-//     .resolve()
-
-
-
-// $(window).on('load', () => {
-//     $(document).on('click', '[data-path]', (e) => {
-//         // e.preventDefault()
-//         // router.navigate($(e.target).attr('href'))
-//         console.log($(e.target).attr('href'))
-//     })
-// })
-// // set the 404 route
-// router.notFound((query) => { $id('view').innerHTML = '<h3>Couldn\'t find the page you\'re looking for...</h3>'; });
-//
-// router.resolve();
-
-// //
-// // console.log(paths)
-// class ViewController {
-//
-//   //view should instiate
-//   // state = new Store({test:"test"})
-//
-//   /** Initialize Application */
-//   constructor () {
-//     this.initializeComponents()
-//     // this.;
-//     // console.log('1',state.getName())
-//
-//
-//
-//   }
-//
-//   initializeComponents () {
-//
-//
-//     // state.setName({test2:"test2"});
-//     // console.log('2', state.getName())
-//     // state.setName({test2:"test3"});
-//     // console.log('8', state.getName())
-//
-//     // Initialize Map
-//     // this.mapComponent = new Map('map-holder',{state:state});
-//     this.mapComponent = new Map('map-holder');
-//     // console.log(this.mapComponent.renderCount);
-//     // console.log(this.mapComponent.overlayMaps)
-//     // Initialize Nav Var
-//     this.navComponent = new nav_bar('nav-holder',{activeNav: 'index'});
-//
-//     // Initialize Layer Toggle Panel
-//     this.maplayersComponent = new MapLayersList('maplayers_list-holder',{
-//       events: { layerToggle:
-//         // Toggle layer in map controller on "layerToggle" event
-//          event => { this.mapComponent.toggleLayer(event.detail) }
-//       }
-//     })
-//   }
-// }
-//
-//
-// window.ctrl = new ViewController()
+window.ctrl = new ViewController();
