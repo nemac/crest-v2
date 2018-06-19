9e26841d59c5592115e02ac96934ba9111bd20be
Dave Michelson <daveism@gmail.com>
2018-06-19 14:15:19 -0400

    fix for navigation going to route directory

--- src/config/navConfig.js
+++ src/config/navConfig.js
@@ -1,20 +1,20 @@
 export var navConfig = {
   navs:[{
-    name: "home" ,
+    name: "home",
     ref: "main-nav-map",
     text: "Home",
     id: "main-nav-map",
     href: "/#Home"
   },
   {
-    name: "download" ,
+    name: "download",
     ref: "main-nav-download",
     text: "Download Data",
     id: "main-nav-download",
     href: "/#Download"
   },
   {
-    name: "about" ,
+    name: "about",
     ref: "main-nav-about",
     text: "About",
     id: "main-nav-about",
