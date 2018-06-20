ff7a8253beeb4bd76980d4c932621503d900a36d
Dave Michelson <daveism@gmail.com>
2018-06-20 14:55:25 -0400

    this fixes #24

--- dist/store.app.bundle.js
+++ dist/store.app.bundle.js
@@ -63,7 +63,7 @@
 /******/ 	}
 /******/
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "d10651c818134abe97a2"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "a9056f52fcae2f043d3a"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -113,8 +113,7 @@
 /******/ 		for (var name in __webpack_require__) {
 /******/ 			if (
 /******/ 				Object.prototype.hasOwnProperty.call(__webpack_require__, name) &&
-/******/ 				name !== "e" &&
-/******/ 				name !== "t"
+/******/ 				name !== "e"
 /******/ 			) {
 /******/ 				Object.defineProperty(fn, name, ObjectFactory(name));
 /******/ 			}
@@ -138,10 +137,6 @@
 /******/ 					}
 /******/ 				}
 /******/ 			}
-/******/ 		};
-/******/ 		fn.t = function(value, mode) {
-/******/ 			if (mode & 1) value = fn(value);
-/******/ 			return __webpack_require__.t(value, mode & ~1);
 /******/ 		};
 /******/ 		return fn;
 /******/ 	}
