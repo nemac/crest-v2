70d62b051381939503b844bcf4f04043653b554b
Dave Michelson <daveism@gmail.com>
2018-07-17 14:04:41 -0400

    build dist

--- dist/bootstrap.app.bundle.js
+++ dist/bootstrap.app.bundle.js
@@ -110,7 +110,7 @@
 /******/ 	}
 /******/
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "35265a57d8d77aac1bb3"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "4f150f656866cd0cbcf2"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
