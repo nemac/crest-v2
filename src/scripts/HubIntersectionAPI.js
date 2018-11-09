4c651717c8a20d11cb9ecd2b7c52069400b51409
Matthew Geiger <mgeiger@unca.edu>
2018-11-09 15:22:23 -0500

    Move hub intersection api settings to config object in src/config

--- src/scripts/HubIntersectionAPI.js
+++ src/scripts/HubIntersectionAPI.js
@@ -1,9 +1,3 @@
-const Terraformer = require('terraformer');
-
-Terraformer.ArcGIS = require('terraformer-arcgis-parser');
-
-const axios = require('axios');
-
 // Use Case
 //
 // Normal Flow:
@@ -15,28 +9,19 @@ const axios = require('axios');
 //  of a geojson feature returned from the zonal stats API (nfwf-tool-api).
 //  When all features have finished processing, return an Array of processed features.
 //
-// Alternate Flow 1:
+// Alternate Flow:
 //  The request for intersection is a success, but no features are returned.
 //  Return an empty Array.
 //
-// Alternate Flow 2:
-//  The Feature Server returns a bad request status code (400).
-//  That's not good.
+// Alternate Flow (Errors):
+//  - The geojson feature is malformed.
+//  - The Feature Server returns a bad request status code (400).
+//  In these cases an Error object is returned.
 
-const fieldMaps = {
-  terrestri: 'terrestrial',
-  crit_fac: 'crit_facilities',
-  pop_dens: 'pop_density',
-  soc_vuln: 'social_vuln',
-  floodprone: 'floodprone_areas',
-  slr: 'sea_level_rise'
-};
-
-const GDB_NAME = 'hubs_with_stats_gdb';
-const SERVICE_URL = `https://services1.arcgis.com/PwLrOgCfU0cYShcG/arcgis/rest/services/${GDB_NAME}/FeatureServer/0`;
-const QUERY_REQUEST_URL = `${SERVICE_URL}/query`;
-
-const agolOutFields = ['exposure', 'asset', 'threat', 'aquatic', 'terrestri', 'hubs', 'crit_infra', 'crit_fac', 'pop_dens', 'soc_vuln', 'drainage', 'erosion', 'floodprone', 'geostress', 'slr', 'slope', 'stormsurge'];
+import config from '../config/hubIntersectionConfig';
+const Terraformer = require('terraformer');
+Terraformer.ArcGIS = require('terraformer-arcgis-parser');
+const axios = require('axios');
 
 
 function transformAgolAttrs(attrs) {
@@ -44,8 +29,8 @@ function transformAgolAttrs(attrs) {
   Object.keys(attrs).forEach((agolField) => {
     let fieldName;
     const val = attrs[agolField];
-    if (agolField in fieldMaps) {
-      fieldName = fieldMaps[agolField];
+    if (agolField in config.fieldMaps) {
+      fieldName = config.fieldMaps[agolField];
     } else {
       fieldName = agolField;
     }
@@ -66,9 +51,9 @@ function convertAgolHubsFeature(feature) {
 
 
 export class HubIntersectionApi {
-  constructor(url = QUERY_REQUEST_URL) {
+  constructor(url = config.queryUrl) {
     this.queryUrl = url;
-    this.agolOutFields = agolOutFields;
+    this.agolOutFields = config.agolOutFields;
   }
 
   async getIntersectedHubs(feature) {
