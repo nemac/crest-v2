799db5f1d70840de587bde9ba20ea75fe5956ef2
Dave Michelson <daveism@gmail.com>
2018-11-12 16:30:42 -0500

    slight adjustment

--- src/scripts/zonalStats.js
+++ src/scripts/zonalStats.js
@@ -619,17 +619,17 @@ function getDriverHeight(driver) {
 }
 
 
-// // Finds the scaled position for the drivers
-// // @param driver | float - value from the api for a driver
-// // @return float - [0,100]
-// function getDriverOneZeroHeight(driver) {
-//   const LOW_RANGE = 0;
-//   const HIGH_RANGE = 1;
-//   const SCALE = 0;
-//   const SCALE_GROUPS = 1;
-//
-//   return getValuePosition(driver, LOW_RANGE, HIGH_RANGE, SCALE, SCALE_GROUPS);
-// }
+// Finds the scaled position for the drivers
+// @param driver | float - value from the api for a driver
+// @return float - [0,100]
+function getDriverOneZeroHeight(driver) {
+  const LOW_RANGE = 0;
+  const HIGH_RANGE = 1;
+  const SCALE = 0;
+  const SCALE_GROUPS = 1;
+
+  return getValuePosition(driver, LOW_RANGE, HIGH_RANGE, SCALE, SCALE_GROUPS);
+}
 
 function getSevenHeight(driver) {
   const LOW_RANGE = 0;
