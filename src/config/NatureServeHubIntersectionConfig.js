ccd8d90dad19e5233c4b8bb6147a6f3d3a2fdb89
Dave Michelson <daveism@gmail.com>
2019-04-03 17:13:02 -0400

    update for support of targeted watershed data.

--- src/config/NatureServeHubIntersectionConfig.js
+++ src/config/NatureServeHubIntersectionConfig.js
@@ -1,16 +1,16 @@
 
-const GDB_NAME = 'NFWF_HUBS_WITH_STATS_CONUS_01072019_gdb';
+const GDB_NAME = 'NS_HUBS_WGS84_CLEAN_WITH_STATS';
 const QUERY_REQUEST_URL = `https://services1.arcgis.com/PwLrOgCfU0cYShcG/arcgis/rest/services/${GDB_NAME}/FeatureServer/0/query`;
 const config = {
   fieldMaps: {
     exposure: 'ns_exposure',
     asset: 'ns_asset',
     threat: 'ns_threat',
-    aquatic: 'ns_fishandwildlife',
-    hubs: 'ns_hubs'
+    fishwild: 'ns_fishandwildlife',
+    hub_rnk: 'ns_hubs'
   },
   queryUrl: QUERY_REQUEST_URL,
-  agolOutFields: [ 'TARGET_FID','exposure', 'asset', 'threat', 'aquatic', 'hubs']
+  agolOutFields: [ 'TARGET_FID','exposure', 'asset', 'threat', 'fishwild', 'hub_rnk']
 };
 
 export default config;
