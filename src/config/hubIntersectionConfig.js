e11410006e2eacc0d2492a01fdea129efe263809
Dave Michelson <daveism@gmail.com>
2020-11-24 15:11:50 -0500

    adds search by hubs for hawaii and downloads

--- src/config/hubIntersectionConfig.js
+++ src/config/hubIntersectionConfig.js
@@ -27,8 +27,7 @@ const config = [
     region: 'continental_us',
     queryUrl: `https://services1.arcgis.com/PwLrOgCfU0cYShcG/ArcGIS/rest/services/CONUS_hubs_staging_073020/FeatureServer/0/query`,
     agolOutFields: [ 'TARGET_FID','exposure', 'asset', 'threat', 'aquatic', 'terrestri', 'wildlife' , 'hub_rnk', 'crit_infra', 'crit_fac', 'pop_dens', 'soc_vuln', 'drainage', 'erosion', 'floodprone', 'geostress', 'slr', 'slope', 'stormsurge']
-  }
-  ,
+  },
   {
     fieldMaps: {
       hub_rnk: 'hubs',
@@ -101,6 +100,57 @@ const config = [
     region: 'northern_mariana_islands',
     queryUrl: `https://services1.arcgis.com/PwLrOgCfU0cYShcG/arcgis/rest/services/CNMI_hubs_staging_073020/FeatureServer/0/query`,
     agolOutFields: [ 'TARGET_FID','exposure', 'asset', 'threat', 'marine', 'terrestri', 'hub_rnk', 'crit_infra', 'crit_fac', 'pop_dens', 'soc_vuln', 'impermeabl', 'erosion', 'floodprone', 'slr', 'low_areas', 'wildlife', 'wave_flood'  ]
+  },
+
+  {
+    fieldMaps: {
+      hub_rnk: 'hubs',
+      exposure: 'exposure',
+      asset: 'asset',
+      marine: 'marine',
+      terrestri: 'terrestrial',
+      crit_infra: 'crit_infra',
+      crit_fac: 'crit_facilities',
+      pop_dens: 'pop_density',
+      soc_vuln: 'social_vuln',
+      impermeabl: 'impermeable',
+      erosion: 'erosion',
+      landslides: 'landslides',
+      floodprone: 'floodprone_areas',
+      slr: 'sea_level_rise',
+      slope: 'slope',
+      wave_flood: 'wave_flooding',
+      wildlife: 'wildlife'
+    },
+    region: 'northern_mariana_islands',
+    queryUrl: `https://services1.arcgis.com/PwLrOgCfU0cYShcG/arcgis/rest/services/CNMI_hubs_staging_073020/FeatureServer/0/query`,
+    agolOutFields: [ 'TARGET_FID','exposure', 'asset', 'threat', 'marine', 'terrestri', 'hub_rnk', 'crit_infra', 'crit_fac', 'pop_dens', 'soc_vuln', 'impermeabl', 'erosion', 'floodprone', 'slr', 'low_areas', 'wildlife', 'wave_flood'  ]
+  },
+  {
+    fieldMaps: {
+      hub_rnk: 'hubs',
+      exposure: 'exposure',
+      asset: 'asset',
+      marine: 'marine',
+      terrestri: 'terrestrial',
+      crit_infra: 'crit_infra',
+      crit_fac: 'crit_facilities',
+      pop_dens: 'pop_density',
+      soc_vuln: 'social_vuln',
+      impermeabl: 'impermeable',
+      erosion: 'erosion',
+      landslides: 'landslides',
+      floodprone: 'floodprone_areas',
+      slr: 'sea_level_rise',
+      slope: 'slope',
+      stormsurge: 'storm_surge',
+      wildlife: 'wildlife',
+      tsunami: 'tsunami'
+    },
+    region: 'hawaii',
+    queryUrl: `https://services1.arcgis.com/PwLrOgCfU0cYShcG/arcgis/rest/services/NFWF_CREST_Hawaii_Hubs_Cores_11242020/FeatureServer/0/query`,
+    agolOutFields: [ 'TARGET_FID','exposure', 'asset', 'threat', 'marine', 'terrestri', 'hub_rnk', 'crit_infra', 'crit_fac', 'pop_dens', 'soc_vuln', 'impermeabl', 'erosion', 'floodprone', 'landslides', 'slr', 'low_areas', 'stormsurge', 'tsunami', 'wildlife'  ]
+    // agolOutFields: [ 'TARGET_FID','exposure', 'asset', 'threat', 'marine', 'terrestri', 'hub_rnk', 'crit_infra', 'crit_fac', 'pop_dens', 'soc_vuln', 'impermeabl', 'erosion', 'floodprone', 'slr', 'low_areas', 'wildlife'  ]
   },
 ];
 
