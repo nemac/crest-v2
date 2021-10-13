2ef4bcbb270075fdb5104fc0b479b0933091e626
Dave Michelson <daveism@gmail.com>
2021-10-13 13:50:59 -0400

    t

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -286,8 +286,8 @@ export class Map extends Component {
         tileSize: layer.tileSize,
         transparent: layer.transparent,
         zIndex: layer.zIndex,
-        maxNativeZoom: layer.maxNativeZoom,
-        // maxNativeZoom: 13,
+        // maxNativeZoom: layer.maxNativeZoom,
+        maxNativeZoom: 13,
         modifyScrollWheel: false,
         detectRetina: true
       });
