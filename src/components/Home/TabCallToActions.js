80fe9aab60cec75e68179852e753826d7ffe951d
Dave Michelson <daveism@gmail.com>
2022-03-15 17:27:12 -0400

    redo on medium screen for tab call to actions

--- src/components/Home/TabCallToActions.js
+++ src/components/Home/TabCallToActions.js
@@ -29,16 +29,16 @@ export default function TabCallToActions() {
   return (
     <Grid container spacing={2} justifyContent="center" alignItems="center" px={0.25} py={0.75}>
 
-      <Grid item xs={12} sm={12} md={3}>
+      <Grid item xs={12} sm={12} md={6} lg={3}>
         <TabCallToActionCard />
       </Grid>
-      <Grid item xs={12} sm={12} md={3}>
+      <Grid item xs={12} sm={12} md={6} lg={3}>
         <TabCallToActionCard />
       </Grid>
-      <Grid item xs={12} sm={12} md={3}>
+      <Grid item xs={12} sm={12} md={6} lg={3}>
         <TabCallToActionCard />
       </Grid>
-      <Grid item xs={12} sm={12} md={3}>
+      <Grid item xs={12} sm={12} md={6} lg={3}>
         <TabCallToActionCard />
       </Grid>
 
