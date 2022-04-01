0673ce793871844e5499bf328c6ed29310e7cdac
Dave Michelson <daveism@gmail.com>
2022-04-01 15:49:45 -0400

    adds targeted watersheds
    also adds a config for iterating the the downloads TODO: break the config out

--- src/components/DataAndReports/DataAndReportsCardHolder.js
+++ src/components/DataAndReports/DataAndReportsCardHolder.js
@@ -18,58 +18,30 @@ State needed
   - Not sure yet
 
 Props
-  - maybe config file
+  - maybe config file - or config json
   - Not sure yet
 */
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import DataAndReportsCard from "./DataAndReportsCard";
 
-// sample config to create all the cards
-// TODO add config file to store all links and names
-//      iterate through json config to automate
-//      config should be imported from config directory
-const sampleRegionConfig = {
-  sampleName: 'Name 1'
-}
 
 export default function DataAndReportsCardHolder(props) {
+  const { cardConfig } = props;
+
   return (
     <Grid container spacing={1} justifyContent="center" alignItems="center" px={0.25} py={0.75}>
-
-      <Grid item xs={12}>
-        <DataAndReportsCard
-          regionName={'American Somoa'}
-          datFileSize={'100 MB'}
-          dataLink={'https://www.google.com/'}
-          NativeLanguageText={'Amerika Samoa Su’esu’ega o Malosi Gafataulima i Nofoaga Tumatāfaga'}
-          NativeLanguageLink={'https://www.google.com/'}
-          EnglishLink={'https://www.google.com/'}/>
-      </Grid>
-
-      <Grid item xs={12}>
-        <DataAndReportsCard
-          regionName={'Continental United States (CONUS)'}
-          datFileSize={'15 GB'}
-          dataLink={'https://www.google.com/'}
-          EnglishLink={'https://www.google.com/'}/>
-      </Grid>
-
-      <Grid item xs={12}>
-        <DataAndReportsCard
-          regionName={'Guam'}
-          datFileSize={'10 MB'}
-          dataLink={'https://www.google.com/'}
-          EnglishLink={'https://www.google.com/'}/>
-      </Grid>
-
-      <Grid item xs={12}>
-        <DataAndReportsCard
-          regionName={'Hawai\'i'}
-          datFileSize={'25 MB'}
-          dataLink={'https://www.google.com/'}
-          EnglishLink={'https://www.google.com/'}/>
-      </Grid>
+    
+      {cardConfig.map(cardData => (
+        <Grid item xs={12} key={cardData.regionName}>
+          <DataAndReportsCard
+            regionName={cardData.regionName}
+            datFileSize={cardData.datFileSize}
+            NativeLanguageText={cardData.NativeLanguageText}
+            dataLink={cardData.dataLink}
+            EnglishLink={cardData.EnglishLink}/>
+        </Grid>
+      ))}
 
     </Grid>
   )
