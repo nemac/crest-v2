f45b44de5fd7e53dc7dd9b9f7f6c116a6eb413ef
seanebum <smatthe2@unca.edu>
2022-05-09 11:28:24 -0400

    now have jest/enzyme working with react 17, all dependencies appear to be sorted and correct.  Also including example snapshot test for example, will probably update/change this later.

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,20 @@
 import React from 'react' ;
 import { shallow } from 'enzyme';
+import { mapConfig } from './configuration/config';
+import { BasicSelect } from './components/Map/basicSelect';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { render } from '@testing-library/react' ;
 import App from './App' ;
-import { BasicSelect } from './components/Map/basicSelect';
 
-//const regionSelector = shallow(<BasicSelect defaultValue={selectedRegion} values={regions} onChange={handleRegionSelectChange}/>)
+Enzyme.configure({ adapter: new Adapter() });
 
-define('Region', () => {
+const regions = mapConfig.regions;
+const regionSelector = shallow(<BasicSelect defaultValue={'Continental U.S.'} values={regions}/>)
+
+
+describe('Region', () => {
     it('Should render the region selector correctly', () => {
-        //expect(regionSelector).toMatchSnapshot() ;
+        expect(regionSelector).toMatchSnapshot() ;
     });
 });
