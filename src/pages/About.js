d545b40ff7dba5994e4790a0c8d50655e36dd862
Dave Michelson <daveism@gmail.com>
2022-05-10 14:58:00 -0400

    fix issues with screen size switching
    also removes unused components

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,14 +10,14 @@ import Paper from '@mui/material/Paper';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 import Typography from '@mui/material/Typography';
-import { makeStyles } from '@mui/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { makeStyles, useTheme } from '@mui/styles';
 
 import AboutTabPannel from '../components/About/AboutTabPannel';
 import AboutCrest from '../components/About/AboutCrest';
 import AboutCommunityExposure from '../components/About/AboutCommunityExposure';
 import AboutFishAndWildlife from '../components/About/AboutFishAndWildlife';
 import AboutResilienceHubs from '../components/About/AboutResilienceHubs';
-import DescriptionCard from "../components/About/DescriptionCard";
 
 import NEMACAboutLogoImage from '../assets/images/nemac_logo_white.png';
 import NFWFAboutLogoImage from '../assets/images/nfwf_logo_white.png';
@@ -90,10 +90,57 @@ export default function AboutTabs (props) {
       setValue(newValue);
   };
 
+  // get breakpoint for small screens so we can force cards on small screen and tabs on larger screens
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+
   const classes = useStyles();
   return (
-    <Box  px={3} py={0.75} >
-        <Grid container spacing={2} justifyContent="center" alignItems="center" px={3} py={0.75} sx={{ display: { xs: 'none', md: 'flex'} }}>
+    <Box px={{ xs: 0, md: 3}} py={0.75} >
+      {isSmallScreen ? (
+        <div>
+        <Grid container spacing={2} justifyContent="center" alignItems="center" px={2} py={1} >
+          <Grid item xs={12} className={classes.item}>
+            <Box  py={0.75} className={classes.descriptionBox} >
+              <Paper variant="outlined" square={false} sx={{minHeight: '800px', height: '100%'}}>
+                <AboutCrest />
+              </Paper>
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} justifyContent="center" alignItems="center" px={2} py={1}  >
+          <Grid item xs={12} className={classes.item}>
+            <Box  py={0.75} className={classes.descriptionBox} >
+              <Paper variant="outlined" square={false} sx={{minHeight: '800px', height: '100%'}}>
+                <AboutCommunityExposure />
+              </Paper>
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} justifyContent="center" alignItems="center" px={2} pt={1} >
+          <Grid item xs={12} className={classes.item}>
+            <Box  py={0.75} className={classes.descriptionBox} >
+              <Paper variant="outlined" square={false} sx={{minHeight: '800px', height: '100%'}}>
+                <AboutFishAndWildlife />
+              </Paper>
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} justifyContent="center" alignItems="center" px={2} py={1} >
+          <Grid item xs={12} className={classes.item}>
+            <Box  py={0.75} className={classes.descriptionBox} >
+              <Paper variant="outlined" square={false} sx={{minHeight: '800px', height: '100%'}}>
+                <AboutResilienceHubs />
+              </Paper>
+            </Box>
+          </Grid>
+        </Grid>
+        </div>
+      ) : (
+        <Grid container spacing={2} justifyContent="center" alignItems="center" px={3} py={0.75} >
           <Grid item xs={12} >
             <Box className={classes.TabPanels}>
                <Box px={2} className={classes.TabHeading}>
@@ -122,48 +169,10 @@ export default function AboutTabs (props) {
              </Box>
           </Grid>
         </Grid>
+      )
+      }
 
-        <Grid container spacing={2} justifyContent="center" alignItems="center" px={4} py={1} sx={{ display: { xs: 'flex', md: 'none'} }} >
-          <Grid item xs={12} className={classes.item}>
-            <Box  py={0.75} className={classes.descriptionBox} >
-              <Paper variant="outlined" square={false} sx={{minHeight: '800px', height: '100%'}}>
-                <AboutCrest />
-              </Paper>
-            </Box>
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={2} justifyContent="center" alignItems="center" px={4} py={1} sx={{ display: { xs: 'flex', md: 'none'} }} >
-          <Grid item xs={12} className={classes.item}>
-            <Box  py={0.75} className={classes.descriptionBox} >
-              <Paper variant="outlined" square={false} sx={{minHeight: '800px', height: '100%'}}>
-                <AboutCommunityExposure />
-              </Paper>
-            </Box>
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={2} justifyContent="center" alignItems="center" px={4} pt={1} sx={{ display: { xs: 'flex', md: 'none'} }} >
-          <Grid item xs={12} className={classes.item}>
-            <Box  py={0.75} className={classes.descriptionBox} >
-              <Paper variant="outlined" square={false} sx={{minHeight: '800px', height: '100%'}}>
-                <AboutFishAndWildlife />
-              </Paper>
-            </Box>
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={2} justifyContent="center" alignItems="center" px={4} py={1} sx={{ display: { xs: 'flex', md: 'none'} }} >
-          <Grid item xs={12} className={classes.item}>
-            <Box  py={0.75} className={classes.descriptionBox} >
-              <Paper variant="outlined" square={false} sx={{minHeight: '800px', height: '100%'}}>
-                <AboutResilienceHubs />
-              </Paper>
-            </Box>
-          </Grid>
-        </Grid>
-
-      <Grid container spacing={2} justifyContent="center" alignItems="center" px={{ xs: 4, sm: 3 }} pt={2} pb={0.75} >
+      <Grid container spacing={2} justifyContent="center" alignItems="center" px={{ xs: 2, md: 3 }} pt={2} pb={0.75} >
         <Grid item xs={12} sm={6} >
           <img src={NFWFAboutLogoImage} className={classes.AboutLogoImageStyle} />
         </Grid>
