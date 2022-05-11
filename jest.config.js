055d8cb2c2290e5e8010ea382851a573f1beed0d
seanebum <smatthe2@unca.edu>
2022-05-11 14:47:34 -0400

    moved assetsTransformer to __tests__/setup dir to clean up root a little

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "^.+\\.(js|jsx)$": "babel-jest",
   },
   moduleNameMapper: { 
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.js", "\\.(css|less)$": "<rootDir>/assetsTransformer.js" 
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.js", "\\.(css|less)$": "<rootDir>/__tests__/setup/assetsTransformer.js" 
     },
   setupFilesAfterEnv: ["<rootDir>__tests__/setup/setupTests.js"],
   testPathIgnorePatterns: ["<rootDir>/__tests__/setup/"]
