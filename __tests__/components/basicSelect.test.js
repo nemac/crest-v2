cc218df88abd150af7871084a7aba29ccb5d2f96
seanebum <smatthe2@unca.edu>
2022-05-12 13:55:50 -0400

    updated example tests for basicSelect and updated dependencies to handle async tests/calls

--- __tests__/components/basicSelect.test.js
+++ __tests__/components/basicSelect.test.js
@@ -1,16 +1,26 @@
 import React from 'react' ;
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { mapConfig } from '../../src/configuration/config';
 import { BasicSelect } from '../../src/components/Map/basicSelect';
+import { changeRegionValue } from '../../src/reducers/regionSelectSlice';
 import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk';
 
+const middlewares = [thunk];
 const initialState = {"value": 'Continental U.S.'}
-const mockStore = configureStore()
+const mockStore = configureStore(middlewares)
 
 let store
 
 const regions = mapConfig.regions;
-const regionSelector = shallow(<BasicSelect defaultValue={'Continental U.S.'} values={regions}/>)
+const mockChangeRegion = jest.fn()
+
+const event = {
+    target: { value: 'the-value' }
+  };
+
+const regionSelector = shallow(<BasicSelect defaultValue={'Continental U.S.'} onChange={mockChangeRegion} values={regions} />)
+{/* <BasicSelect defaultValue={selectedRegion} values={regions} onChange={handleRegionSelectChange}/> */}
 
 describe('Region', () => {
     beforeEach(() => {
@@ -18,6 +28,15 @@ describe('Region', () => {
     })
     it('Should render the region selector correctly', () => {
         expect(regionSelector).toMatchSnapshot() ;
-        console.log(regionSelector.props)
     });
+    it('Should have the correct initial state', () => {
+        // console.log(regionSelector.find('select').prop())
+        expect(regionSelector.find('select').prop('defaultValue')).toBe('Continental U.S.') ;
+    })
+    it('Calls the onChange handler', () => {
+        // const component = regionSelector.dive()
+        regionSelector.find('select').simulate('change', event);
+        expect(mockChangeRegion).toBeCalledWith(event);
+
+    })
 });
--- babel.config.js
+++ babel.config.js
@@ -1,2 +1,5 @@
-module.exports = {presets: ['@babel/preset-env', '@babel/preset-react']}
+module.exports = {
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: ["@babel/transform-runtime"]
+}
 
