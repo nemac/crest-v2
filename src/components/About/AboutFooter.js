76da721c9495db4c21ea3573ed489c7b53983a77
Dave Michelson <daveism@gmail.com>
2022-05-16 17:10:17 -0400

    on last fix for small screens

--- src/components/About/AboutFooter.js
+++ src/components/About/AboutFooter.js
@@ -28,7 +28,10 @@ import NFWFAboutLogoImage from '../../assets/images/nfwf_logo_white.png';
 
 const useStyles = makeStyles((theme) => ({
   AboutLogoImageStyle: {
-    maxHeight: '64px'
+    maxHeight: '80px',
+    maxWidth: '100%',
+    height: 'auto',
+    padding: theme.spacing(1)
   }
 }));
 
@@ -38,10 +41,10 @@ export default function AboutFooter(props) {
   return (
     <Grid container spacing={2} justifyContent="center" alignItems="center" px={{ xs: 2, md: 3 }} pt={2} pb={0.75} >
       <Grid item xs={12} sm={6} >
-        <img src={NFWFAboutLogoImage} className={classes.AboutLogoImageStyle} />
+        <img src={NFWFAboutLogoImage} className={classes.AboutLogoImageStyle} px={1}/>
       </Grid>
       <Grid item xs={12} sm={6} >
-        <img src={NEMACAboutLogoImage} className={classes.AboutLogoImageStyle} />
+        <img src={NEMACAboutLogoImage} className={classes.AboutLogoImageStyle} px={1}/>
       </Grid>
     </Grid>
   );
