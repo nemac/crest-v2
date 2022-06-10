26259046f451e7d044fba7e13899fbbdc378e549
seanebum <smatthe2@unca.edu>
2022-06-10 12:33:06 -0400

    apparently adding keys for this fixed the issue of layers not appearing to remove in order until refresh... weird

--- src/components/Map/ActiveTileLayers.js
+++ src/components/Map/ActiveTileLayers.js
@@ -5,8 +5,8 @@ import { TileLayer } from 'react-leaflet'
 export default function ActiveTileLayers() {
     
     const activeTileURLS = useSelector((state) => state.mapLayerList.activeLayerList)
-    
+
   return (
-      activeTileURLS.map(layer => <TileLayer url={layer.url}/>)
+      activeTileURLS.map(layer => <TileLayer key={layer.label} url={layer.url}/>)
   )
 }
