ec4b5414cdb1e283f02874699ae87c811c20ecb9
seanebum <smatthe2@unca.edu>
2022-06-23 13:23:11 -0400

    layer descriptions now replicate click behavior from crest v1... clicking the icon will cause the description to remain visible until clicking again (anywhere).  This does seem to trigger the click away event too many times... it does not effect behavior, but is annoying that its happening

--- src/components/MapLayerList/LayerDescription.js
+++ src/components/MapLayerList/LayerDescription.js
@@ -2,53 +2,65 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import InfoIcon from '@mui/icons-material/Info';
 import { IconButton, Typography, Popover } from '@mui/material';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
 
 export default function LayerDescription(props) {
   const { layerName, layerDescription } = props;
   const [anchorEl, setAnchorEl] = useState(null);
+  const [clicked, setClicked] = useState(false);
 
   const handlePopoverOpen = (e) => {
     setAnchorEl(e.currentTarget);
   };
   const handlePopoverClose = () => {
+    if (!clicked) {
+      setAnchorEl(null);
+    }
+  };
+  const handleClickAway = () => {
     setAnchorEl(null);
+    setClicked(false);
+  };
+  const handleClick = () => {
+    setClicked(!clicked);
   };
 
   const open = Boolean(anchorEl);
 
   return (
-    <div>
+    <ClickAwayListener onClickAway={handleClickAway}>
       <IconButton
         variant='contained'
         aria-owns={open ? 'mouse-over-popover' : undefined}
         aria-haspopup="true"
         onMouseEnter={handlePopoverOpen}
         onMouseLeave={handlePopoverClose}
+        onClick={handleClick}
       >
         <InfoIcon />
-      </IconButton>
-      <Popover
-        id="mouse-over-popover"
-        sx={{
-          pointerEvents: 'none'
-        }}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left'
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left'
-        }}
-        onClose={handlePopoverClose}
-        disableRestoreFocus
-      >
-        <h3>{layerName}</h3>
-        <Typography sx={{ p: 1 }}>{layerDescription}</Typography>
-      </Popover>
-    </div>
+        <Popover
+          id="mouse-over-popover"
+          sx={{
+            pointerEvents: 'none'
+          }}
+          open={open}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left'
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left'
+          }}
+          onClose={handlePopoverClose}
+          disableRestoreFocus
+        >
+          <h3>{layerName}</h3>
+          <Typography sx={{ p: 1 }}>{layerDescription}</Typography>
+        </Popover>
+        </IconButton>
+      </ClickAwayListener>
   );
 }
 LayerDescription.propTypes = {
