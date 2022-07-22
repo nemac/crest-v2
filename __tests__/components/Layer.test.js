daeae6b7d978b654520eb1300843de03239653cc
seanebum <smatthe2@unca.edu>
2022-07-22 12:11:37 -0400

    cleaned up tests a little

--- __tests__/components/Layer.test.js
+++ __tests__/components/Layer.test.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Layer from '../../src/components/MapLayerList/Layer'
-import { render, fireEvent, screen } from '../setup/testUtils';
+import { render, fireEvent, screen, cleanup } from '../setup/testUtils';
 import { mapConfig } from '../../src/configuration/config';
 
 const regions = mapConfig.regions;
 
 describe('Layer', () => {
+    afterEach(() => {
+        cleanup();
+    });
     it('renders', () => {
         render(<Layer layerData={regions['American Samoa'].layerList[0]}/>)
-    })
+    });
     it('clicks on and off', () => {
         render(<Layer layerData={regions['American Samoa'].layerList[0]}/>)
         expect(screen.getByRole('checkbox').checked).toBe(false)
@@ -16,14 +19,13 @@ describe('Layer', () => {
         expect(screen.getByRole('checkbox').checked).toBe(true)
         fireEvent.click(screen.getByRole('checkbox'))
         expect(screen.getByRole('checkbox').checked).toBe(false)
-    })
+    });
     it('updates the store', () => {
         const expected = regions['American Samoa'].layerList[0].id ;
         const { store } = render(<Layer layerData={regions['American Samoa'].layerList[0]}/>)
         expect(store.getState().mapLayerList.activeLayerList).toMatchObject({})
         fireEvent.click(screen.getByRole('checkbox'))
         expect(store.getState().mapLayerList.activeLayerList).toHaveProperty(expected)
-        
-    })
-})
+    });
+});
 
--- __tests__/components/LayerDescription.test.js
+++ __tests__/components/LayerDescription.test.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { render } from '../setup/testUtils';
 import LayerDescription from '../../src/components/MapLayerList/LayerDescription';
-import { ExpandCircleDownOutlined } from '@mui/icons-material';
 import { screen, cleanup, fireEvent } from '@testing-library/react';
 
 describe('LayerDescription', () => {
     beforeEach(() => {
         render(<LayerDescription layerName='test' layerDescription='this is a test'/>);
-    })
+    });
     afterEach(() => {
         cleanup();
-    })
+    });
     it('renders', () => {
-    })
+    });
     it('displays the right title', async () => {
         fireEvent.mouseEnter(screen.getByRole('button'));
         expect(await screen.findByText('test')).toBeDefined();
-    })
+    });
     it('displays the right description', async () => {
         fireEvent.mouseEnter(screen.getByRole('button'));
         expect(await screen.findByText('this is a test')).toBeDefined();
-    })
+    });
 })
 
