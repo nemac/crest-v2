5dd12ece993f4ea2843493895c559f782328ab95
seanebum <smatthe2@unca.edu>
2022-07-27 14:58:56 -0400

    added test for Layer Group

---
+++ __tests__/components/LayerGroup.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { screen, cleanup, fireEvent } from '@testing-library/react';
+import { render } from '../setup/testUtils';
+import { mapConfig } from '../../src/configuration/config';
+import LayerGroup from '../../src/components/MapLayerList/LayerGroup';
+
+const regions = mapConfig.regions;
+const testLayerList = regions['American Samoa'].layerList;
+
+describe('LayerGroup', () => {
+  beforeEach(() => {
+    render(<LayerGroup chartLayerList={testLayerList} />).store;
+  });
+  afterEach(() => {
+    cleanup();
+  });
+  it('renders', () => {
+  });
+  it('displays layers', () => {
+      // This checks that both the subheading and Layer exist for 'Resilience Hubs'
+      expect(screen.getAllByText(testLayerList[0].label)).toHaveLength(2);
+  })
+
+});
