28d18d002eecdae82354606512724c8c9449e3af
nadiafourie <115162892+nadiafourie@users.noreply.github.com>
2023-01-17 15:09:12 -0500

    LayerLegend testing
    Testing file for LayerLegend

--- __tests__/components/LayerLegend.test.js
+++ __tests__/components/LayerLegend.test.js
@@ -4,21 +4,33 @@ import LayerLegend from '../../src/components/MapLayerList/LayerLegend';
 import { screen, cleanup } from '@testing-library/react';
 import { mapConfig } from '../../src/configuration/config';
 
-
 const regions = mapConfig.regions;
 const testLayerList = regions['American Samoa'].layerList;
 const testLayer = testLayerList[0];
 
 describe('LayerLegend', () => {
+
     beforeEach(() => {
         render(<LayerLegend layer={testLayer}/>);
     });
     afterEach(() => {
         cleanup();
     });
-    it('renders', () => {
-        expect(screen.getByText('Low')).toBeInTheDocument();
-        expect(screen.getByText('High')).toBeInTheDocument();
-    });
-})
 
+    describe('Colours and format', () => {
+
+        test('ColorChart not empty', () => {
+            expect(testLayer.chartCSSLegends).not.toBeNull()
+        })
+
+        test('Function pickCSSBasedBgColor works', () => {
+            expect(testLayer.color).not.toBeNull()
+
+        })
+
+        it('Should render as expected', () => {
+            expect(screen.getByText('Low')).toBeInTheDocument();
+            expect(screen.getByText('High')).toBeInTheDocument();
+        });
+    })
+})
