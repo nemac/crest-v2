fd46ab428f379bb8161d2241bd5d7505d50d02ab
seanebum <smatthe2@unca.edu>
2023-03-07 10:44:54 -0500

    words are now displayed 1 per line in x tick labels

--- src/components/AnalyzeArea/ChartInteractiveLabels.js
+++ src/components/AnalyzeArea/ChartInteractiveLabels.js
@@ -4,11 +4,19 @@ export default function ChartInteractiveLabels(props) {
   const {
     x, y, stroke, payload
   } = props;
+  const words = payload.value.match(/\b(\w+)\b/g);
+  const textWithOffset = {};
+  const vertHeight = 10;
+  words.map((word, index) => getText(textWithOffset, word, index, vertHeight));
+
   return (
-        <g transform={`translate(${x},${y})`} onClick={() => console.log(payload.value)}>
-            <text x={0} y={0} dy={16} style={{ fontSize: '10px' }} textAnchor="middle" fill="#FFF">
-                {payload.value}
-            </text>
+        <g transform={`translate(${x},${y})`} onClick={() => console.log(test)}>
+            {Object.entries(textWithOffset).map(([word, offset]) => <text key={word} x={0} y={0} dy={16} style={{ fontSize: '10px' }} textAnchor="middle" fill="#FFF">
+                <tspan textAnchor="middle" x="0" dy={offset}>{word}</tspan>
+            </text>)}
         </g>
   );
+}
+function getText(textWithOffset, word, index, vertHeight) {
+  textWithOffset[word] = index * vertHeight;
 }
