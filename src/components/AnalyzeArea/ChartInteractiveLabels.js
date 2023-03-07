2ea09664474e690db22320020479e7ed15d59d33
seanebum <smatthe2@unca.edu>
2023-03-07 10:45:42 -0500

    fixed onClick to not error again

--- src/components/AnalyzeArea/ChartInteractiveLabels.js
+++ src/components/AnalyzeArea/ChartInteractiveLabels.js
@@ -10,7 +10,7 @@ export default function ChartInteractiveLabels(props) {
   words.map((word, index) => getText(textWithOffset, word, index, vertHeight));
 
   return (
-        <g transform={`translate(${x},${y})`} onClick={() => console.log(test)}>
+        <g transform={`translate(${x},${y})`} onClick={() => console.log(payload.value)}>
             {Object.entries(textWithOffset).map(([word, offset]) => <text key={word} x={0} y={0} dy={16} style={{ fontSize: '10px' }} textAnchor="middle" fill="#FFF">
                 <tspan textAnchor="middle" x="0" dy={offset}>{word}</tspan>
             </text>)}
