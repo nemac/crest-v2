f9283cd5508352e319ddc93a6e6358bd21cfc809
seanebum <smatthe2@unca.edu>
2023-03-09 15:50:54 -0500

    attributions are now being shown correctly on refresh, it looks like... still trying to figure out a way around a full basemap re-render

--- src/components/Map/ActiveTileLayers.js
+++ src/components/Map/ActiveTileLayers.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { TileLayer } from 'react-leaflet';
+import { updateAttributions } from '../../reducers/mapPropertiesSlice';
+
+const activeLayerListSelector = (state) => state.mapLayerList.activeLayerList;
+const attributionsSelector = (state) => state.mapProperties.attributions;
 
 export default function ActiveTileLayers() {
-  const activeLayerListSelector = (state) => state.mapLayerList.activeLayerList;
+  const dispatch = useDispatch();
+  const handleAttributionsChange = useCallback((newAttributions) => {
+    if (currAttributions.join(', ') !== newAttributions.join(', ')) {
+      console.log('mismatch!');
+      console.log(`${newAttributions} != ${currAttributions}`);
+      console.log('dispatching ' + newAttributions.join('') + ' to redux state');
+      dispatch(updateAttributions(newAttributions));
+    }
+  });
+  const currentAttributions = useSelector(attributionsSelector);
   const layerList = useSelector(activeLayerListSelector);
+  const layerAttributions = new Set(); // This should become redux State and read in BasemapLayer.js
+  const layers = Object.values(layerList).map((lyr) => {
+    layerAttributions.add(lyr.attribution);
+    return (<TileLayer key={lyr.id} url={lyr.url} opacity={lyr.opacity} pane={'overlayPane'} maxNativeZoom={lyr.maxNativeZoom} />
+    );
+  });
+  const currAttributions = [...currentAttributions].sort();
+  const newAttributions = Array.from(layerAttributions).sort();
 
+  useEffect(() => {
+    handleAttributionsChange(newAttributions);
+  }, [handleAttributionsChange, newAttributions]);
   return (
     // eslint-disable-next-line max-len
-    Object.values(layerList).map((lyr) => <TileLayer key={lyr.id} url={lyr.url} attribution={lyr.attribution} opacity={lyr.opacity} maxNativeZoom={lyr.maxNativeZoom}/>)
+    layers
   );
 }
--- src/components/Map/BasemapLayer.js
+++ src/components/Map/BasemapLayer.js
@@ -4,16 +4,45 @@ import React, {
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { vectorBasemapLayer } from 'esri-leaflet-vector';
+import { AttributionControl } from 'react-leaflet';
 import { mapConfig } from '../../configuration/config';
 
 const basemaps = mapConfig.basemaps;
 
 const baseMapSelector = (state) => state.mapProperties.basemap;
+const attributionsSelector = (state) => state.mapProperties.attributions;
 
 export default function BasemapLayer(props) {
   const { map } = props;
   const selectedBasemap = useSelector(baseMapSelector);
+  const selectedAttributions = useSelector(attributionsSelector);
+  const attributionString = useRef('');
   const basemapRef = useRef(null);
+  const handleAttributionsChange = useCallback((newAttributions) => {
+    // console.log('attributions change detected in basemap layer');
+    // console.log('selected attributions: ' + selectedAttributions);
+
+    // console.log('attributionString was ' + attributionString.current);
+    // console.log('size : ', selectedAttributions.length);
+    // if (map !== null) {
+    //   console.log(map);
+    //   map.attributionControl.addAttribution('sup');
+    // }
+    attributionString.current = selectedAttributions.length > 0 ? Array.from(selectedAttributions).join(', ') : '';
+    // console.log('setting attributionString to ' + attributionString.current);
+    // if (basemapRef.current !== null) {
+      // basemapRef.current.options.attribution = attributionString.current;
+      // console.log('before: ', basemapRef.current.options);
+      // basemapRef.current.attribution = attributionString.current;
+      // basemapRef.current._setupAttribution();
+      // handleBasemapChange(basemapRef.current);
+      // console.log(basemapRef.current);
+      // map.attributionControl.addAttribution(basemapRef.current.attribution);
+    // }
+    // console.log(basemapRef.current);
+    // basemapRef.current.attribution = attributionString.current;
+  }, [selectedAttributions, map]);
+
   const handleBasemapChange = useCallback((basemapName) => {
     if (basemapRef.current !== null) {
       basemapRef.current.remove(map);
@@ -21,19 +50,24 @@ export default function BasemapLayer(props) {
     if (map) {
       const newBasemap = vectorBasemapLayer(basemaps[basemapName], {
         apikey: 'AAPKa0a45bdbd847441badbdcf07a97939bd0Y1Vpjt3MU7qyu7R9QThGqpucpKmbVXGEdmQo1hqhdjLDKA2zrwty2aeDjT-7-By',
-        pane: 'mapPane'
+        pane: 'mapPane',
+        attribution: attributionString.current // SHOULD PASS IN OUR SET FROM ACTIVE TILE LAYERS
       });
       newBasemap.addTo(map);
       basemapRef.current = newBasemap;
     }
-  }, [map, basemapRef]);
+  }, [map, basemapRef, attributionString]);
+
+  useEffect(() => {
+    handleAttributionsChange(selectedAttributions);
+  }, [handleAttributionsChange, selectedAttributions]);
 
   useEffect(() => {
     handleBasemapChange(selectedBasemap);
   }, [selectedBasemap, handleBasemapChange]);
 
   return (
-        <div></div>
+    <AttributionControl />
   );
 }
 
