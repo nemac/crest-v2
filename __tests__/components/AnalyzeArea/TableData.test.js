ad906e9ab88e3a7247b28ce546caeefdda333a07
nadiafourie <115162892+nadiafourie@users.noreply.github.com>
2023-03-10 15:43:22 -0500

    TableData

--- __tests__/components/AnalyzeArea/TableData.test.js
+++ __tests__/components/AnalyzeArea/TableData.test.js
@@ -5,9 +5,14 @@ import { mapConfig } from '../../../src/configuration/config';
 import TableData from '../../../src/components/AnalyzeArea/TableData';
 
 //Still finishing this one
+
+const chartTest = new Array(1);
+
+chartTest[0] = ["test", "test", 101, "test"];
+
 describe('TableData', () => {
   beforeEach(() => {
-    render(<TableData />).store;
+    render(<TableData data = {chartTest} />).store;
   });
   afterEach(() => {
     cleanup();
@@ -15,7 +20,7 @@ describe('TableData', () => {
 
   describe('Renders as expected: ', () => {
     test('Screen as expected: ', () => {
-        //expect(map.getAllByRole('button')).toBeInTheDocument();
+        expect(screen.getAllByRole('button')).toBeInTheDocument();
     })
   })
 })
