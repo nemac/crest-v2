394f17d26e1c4a96593734dd6c10704a4e328c65
seanebum <smatthe2@unca.edu>
2023-05-11 11:54:29 -0400

    some updates to the search button - still cant get it to collapse to the search button as intended, and also need to discover exactly what I need to do to add the drawn layer/marker to state appropriately along with generating zonal stats data

--- src/components/Map/SearchPlaces.js
+++ src/components/Map/SearchPlaces.js
@@ -20,73 +20,129 @@ State needed
 Props
   Not sure yet
 */
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Popup } from 'react-leaflet';
 import L from 'leaflet';
-import { Button, IconButton } from '@mui/material/';
-
+import { Button } from '@mui/material/';
+import AddchartIcon from '@mui/icons-material/Addchart';
 import { geosearch } from 'esri-leaflet-geocoder';
 import * as ELG from 'esri-leaflet-geocoder';
-import ShowIdentifyPopup from './Identify';
 import { makeStyles } from '@mui/styles';
 import InfoIcon from '@mui/icons-material/Info';
+import ShowIdentifyPopup from './Identify';
+import {
+  addNewFeatureToDrawnLayers,
+  uploadedShapeFileGeoJSON,
+  addNewFeatureToZonalStatsAreas
+} from '../../reducers/mapPropertiesSlice';
 
 export default function SearchPlaces(props) {
-  const { map } = props;
+  const { map, leafletDrawFeatureGroupRef } = props;
+  const dispatch = useDispatch();
+  const apiKey = 'AAPKa0a45bdbd847441badbdcf07a97939bd0Y1Vpjt3MU7qyu7R9QThGqpucpKmbVXGEdmQo1hqhdjLDKA2zrwty2aeDjT-7-By';
+  const GeoSearchOptions = {
+    providers: [
+      ELG.arcgisOnlineProvider({
+        // API Key to be passed to the ArcGIS Online Geocoding Service
+        apikey: apiKey
+      })
+    ],
+    useMapBounds: false,
+    expanded: false,
+    placeholder: 'Search for places or addresses',
+    collapseAfterResult: false,
+    allowMultipleResults: false,
+    attribution: 'Powered by ESRI'
+  };
 
   let identifyData = null;
-  const apiKey = 'AAPKa0a45bdbd847441badbdcf07a97939bd0Y1Vpjt3MU7qyu7R9QThGqpucpKmbVXGEdmQo1hqhdjLDKA2zrwty2aeDjT-7-By';
   const [popupContent, setPopupContent] = useState(null);
+
   const searchControlRef = useRef(null);
+  // const searchLocation = useRef(null);
 
+  function circleToFeature(circle) {
+    const center = circle.getLatLng();
+    const radius = circle.getRadius();
+    const feature = {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [center.lng, center.lat]
+      },
+      properties: {
+        radius,
+        areaName: 'TESTAREA'
+      }
+    };
+    return feature;
+  }
 
-  const handleOnSearchResuts = (data) => {
+  const handleOnSearchResuts = useCallback((data) => {
     console.log('Search results', data);
     identifyData = data.latlng;
     console.log(identifyData);
+
+    const handleGetAreaStatistics = (data) => {
+      const thisArea = L.circle(identifyData, { radius: 1000 });
+      thisArea.addTo(map);
+      const newFeature = circleToFeature(thisArea);
+      // newLayer.addTo(map);
+
+      let layer = L.geoJSON(newFeature);
+      const layerId = L.stamp(layer);
+      newFeature.properties.leafletId = layerId;
+      const leafletIdsList = [layerId];
+      leafletDrawFeatureGroupRef.current.addLayer(layer);
+      // layer.bindTooltip('TESTAREATT', { direction: 'center', permanent: true });
+      if (newFeature.properties.buffer) {
+        // layer = buffer(layer.toGeoJSON(), bufferSize, { units: bufferUnits });
+        // layer = L.geoJSON(layer, { style: bufferStyle });
+        // const bufferLayerId = L.stamp(layer);
+        // newFeature.properties.bufferLayerId = bufferLayerId;
+        // leafletIdsList.push(bufferLayerId);
+        leafletDrawFeatureGroupRef.current.addLayer(layer);
+      }
+      newFeature.properties.leafletIds = leafletIdsList;
+      // dispatch(addNewFeatureToDrawnLayers(newFeature));
+      // dispatch(addNewFeatureToZonalStatsAreas(newFeature));
+      dispatch(uploadedShapeFileGeoJSON(newFeature));
+      console.log(newFeature);
+      searchControlRef.current.clear();
+
+      // return(
+      //   <Circle center={identifyData} radius={1000} />
+      // )
+    };
 
     setPopupContent(
       <Popup position={identifyData} onClose={() => setPopupContent(null)}>
         <div>
           <h2>{data.results[0].text}</h2>
-          <p><IconButton
+          <p><Button
             variant="contained"
-            aria-label="Close"
-            color="CRESTSecondary"
-            onClick={(() => console.log('clicked identify!'))}>
-            <InfoIcon />
-          </IconButton>
-            <Button
-              variant="contained"
-              color="CRESTPrimary"
-              onClick={(() => console.log('clicked explore!'))}
-            >Explore</Button></p>
+            color="CRESTPrimary"
+            onClick={handleGetAreaStatistics}
+          ><AddchartIcon /> Get Statistics for this location</Button></p>
         </div>
       </Popup>
     );
-  };
+  }, []);
   useEffect(() => {
     if (!map) { return; }
 
     if (!searchControlRef.current) {
-      const searchControl = geosearch({
-        providers: [
-          ELG.arcgisOnlineProvider({
-            // API Key to be passed to the ArcGIS Online Geocoding Service
-            apikey: apiKey
-          })
-        ]
-      });
+      const searchControl = geosearch(GeoSearchOptions);
       searchControl.addTo(map);
       searchControlRef.current = searchControl;
 
-
       searchControlRef.current.on('results', handleOnSearchResuts);
-      return () => {
-        searchControlRef.current.off('results', handleOnSearchResuts);
-      }
+      // return () => {
+        // searchControlRef.current.off('results', handleOnSearchResuts);
+      // };
     }
-  }, [handleOnSearchResuts, map]);
+  }, [GeoSearchOptions, handleOnSearchResuts, map]);
 
   return (
     <>
