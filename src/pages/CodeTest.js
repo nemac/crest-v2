a32949c13e2cb63bad7843bca11f37dd480c2efb
Jeff Bliss <jeff.a.bliss@gmail.com>
2023-06-22 12:42:51 -0400

    can no longer draw self intersecting polygons

--- src/pages/CodeTest.js
+++ src/pages/CodeTest.js
@@ -1,36 +1,33 @@
-// This is just for code testing snippet purposes.
-// Can delete when ready for production
 import React from 'react';
-import DialogPopup from '../components/All/DialogPopup';
+import { MapContainer, TileLayer, FeatureGroup } from 'react-leaflet';
+import { EditControl } from "react-leaflet-draw";
 
-export default function CodeTest() {
-  const [a, setA] = React.useState(0);
-  const [error, setError] = React.useState(false);
-
-  const handleErrorMessageClose = () => {
-    setError(false);
-  };
-  if (a === 5) {
-    setError(true);
-    setA(0);
-  }
-  if (error) {
-    return (
-      <DialogPopup
-        contentMessage={'ERROR'}
-        buttonMessage='Okay'
-        onClose={handleErrorMessageClose}
-        open={error}
-      />
-    );
-  }
+function App() {
   return (
-    <div>
-      <button onClick={() => setA(a + 1)}>
-        Increase
-      </button> <br></br>
-      Value of A: {a} <br></br>
-      Error: {error}
-    </div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" />
+      <link rel="stylesheet" href="https://unpkg.com/leaflet-draw@latest/dist/leaflet.draw-src.css" />
+      <FeatureGroup>
+        <EditControl
+          position='topright'
+          draw={{
+            rectangle: false,
+            circle: false,
+            circlemarker: false,
+            marker: false,
+            polyline: false,
+            polygon: {
+              allowIntersection: false,  // Prevent user from drawing self-intersecting polygons
+            }
+          }}
+        />
+      </FeatureGroup>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
   );
 }
+
+export default App;
