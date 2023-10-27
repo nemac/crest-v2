a817512352027e59a6db8d5bdce435a78e79a6d1
Jeff Bliss <jeff.a.bliss@gmail.com>
2023-10-27 13:11:02 -0400

    push this up

--- src/components/AnalyzeArea/ResiliencePieChart.js
+++ src/components/AnalyzeArea/ResiliencePieChart.js
@@ -31,8 +31,9 @@ export default function ResiliencePieChart(props) {
     chartType
   } = props;
   const renderCustomizedLabel = ({
-    cx, cy, midAngle, innerRadius, outerRadius, percent, index
+    entry, cx, cy, midAngle, innerRadius, outerRadius, percent, index
   }) => {
+    console.log('jeff', entry);
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
