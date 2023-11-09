ef78fdfaf4ce24f2afa95baa4b7518cd56f982f2
seanebum <smatthe2@unca.edu>
2023-11-09 11:52:31 -0500

    updated polygon validation to detect too many vertices

--- src/utility/utilityFunctions.js
+++ src/utility/utilityFunctions.js
@@ -7,14 +7,32 @@ export const calculateAreaOfPolygon = ((geojson) => {
   return area;
 });
 
+function isPolygonTooComplex(polygon, maxVertices = 1000) {
+  if (polygon && polygon.geometry && polygon.geometry.type === 'Polygon') {
+    const coordinates = polygon.geometry.coordinates;
+    let totalVertices = 0;
+
+    coordinates.forEach((ring) => {
+      totalVertices += ring.length;
+    });
+    if (totalVertices > maxVertices) {
+      return true;
+    }
+    return false;
+  }
+
+  return false; // Return false for non-polygon geometries or missing data
+}
+
 /* this function determines if the polygon is valid per our checks:
   1. max size is under 500 sq km
+  2. polygon has less than 1000 vertices
 */
 // TODO: Need to add more validation checks e.g. number of vertices
 export const validPolygon = ((geojson) => {
   const area = calculateAreaOfPolygon(geojson);
   const maxPolygonAreaSize = 500000000; // 500 sq km
-  if (area > maxPolygonAreaSize) {
+  if (area > maxPolygonAreaSize || isPolygonTooComplex(geojson)) {
     return false;
   }
   return true;
