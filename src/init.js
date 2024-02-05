f5f206a88a99320736ac84490c6d0a0b4a8c338c
Jeff Bliss <jeff.a.bliss@gmail.com>
2024-02-05 14:35:51 -0500

    more updates for merge

--- src/init.js
+++ src/init.js
@@ -1,2 +1,7 @@
+// https://github.com/vitejs/vite/discussions/2785#discussioncomment-3579481
+import { Buffer } from 'buffer/';
+
+window.Buffer = Buffer;
+
 // https://stackoverflow.com/questions/72114775/vite-global-is-not-defined
 window.global ||= window;
