94c7b520ec01fb5bb975b4a463d1ec418f074640
Dave Michelson <daveism@gmail.com>
2025-03-28 15:44:09 -0400

    adds analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import ReactGA from "react-ga4";
 
 import App from "./App.jsx";
 import { store } from "./store";
 import { saveState } from "./localStorage";
+
+ReactGA.initialize("G-2E98LXVQPJ");
+ReactGA.send("pageview");
 
 // here we subscribe to the store changes
 store.subscribe(() => {
